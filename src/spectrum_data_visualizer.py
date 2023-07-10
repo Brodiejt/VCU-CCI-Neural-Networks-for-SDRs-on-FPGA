6cb05888ef43e8327160c0194db12332f942f092
brodiejt <brodiejt@egr-u-cs-cl-05.rams.adp.vcu.edu>
2023-07-10 15:55:19 -0400

    fixed visualization script'

--- src/spectrum_data_visualizer.py
+++ src/spectrum_data_visualizer.py
@@ -6,8 +6,10 @@ import os
 import matplotlib.pyplot as plt
 import io
 import time
+import scipy.signal as signal
+import scipy.optimize as opt
 
-samples_file = 'IQ_Samples\Samples(Splotches)'
+samples_file = 'IQ_Samples/Samples(Splotches)'
 
 SAMPLE_RATE = 5.88e6
 CENTER_FREQUENCY = 0
@@ -41,6 +43,7 @@ def readSamplesFromFile(filePath):
         fig, ax = plt.subplots()
         xdata, ydata = [], []
         ln, = ax.plot([], [], 'ro-')
+        ln2, = ax.plot([], [], 'blue')
 
         plt.ylabel('Power (dB)')
         plt.xlabel('Frequency (Hz)')
@@ -57,26 +60,46 @@ def readSamplesFromFile(filePath):
 
             print(len(fft_window_samples[index]))
 
-            array = np.array(getFFT_PSD(
-                fft_window_samples[index], 1024, SAMPLE_RATE))
+            ft, Pxx_den = signal.periodogram(fft_window_samples[index], SAMPLE_RATE, 'flattop', scaling='spectrum')
+            Pxx_den_dB = 10.0*np.log10(Pxx_den)
+            alpha = normalize_fft_windows(Pxx_den_dB)
+            ln.set_xdata(ft)
+            ln.set_ydata(Pxx_den_dB)
 
-            xdata = f
-            ydata = array
+            ln2.set_xdata(ft)
 
-            ln.set_xdata(xdata)
-            ln.set_ydata(ydata)
+            x = np.linspace(0, 1, 1024)
 
+            for i in range(x.size):
+                x[i] = func(x[i], alpha[0], alpha[1], alpha[2], alpha[3], alpha[4])
+            
+            ln2.set_ydata(x)
             ax.relim()
             ax.autoscale_view()
 
             fig.canvas.draw()
-            time.sleep(0.05)
+            time.sleep(10)
             fig.canvas.flush_events()
+
+            
+
             index += 1
 
     # Unpack 2 floats for each complex number
     ## complex(data[i], data[i+1])
     return 1
+def func(x, a, b, c, d, e):
+    return a*x**4 + b*x**3 + c*x**2 + d*x + e
 
+def normalize_fft_windows(window):
+    q3, q1 = np.percentile(window, [75, 25])
+    noise_iqr = q3-q1
+    median = np.median(window)
+    noise_ceiling = median+noise_iqr
+    print(f'-----\nNoise IQR: {noise_iqr} \nmedian: {median}\nNoise Ceiling: {noise_ceiling}\n ')
+
+    array = opt.curve_fit(func, xdata = np.linspace(0, 1, 1024), ydata = window)[0]
+    print(array)
+    return array
 
 readSamplesFromFile(samples_file)
