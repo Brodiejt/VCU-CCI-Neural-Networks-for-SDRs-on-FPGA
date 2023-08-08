6afc007e42988f39aae527727e812e297739d3eb
Brodiejt <brodiejt@vcu.edu>
2023-08-08 13:43:02 -0400

    assistance needed

--- src/gru/src/main.cpp
+++ src/gru/src/main.cpp
@@ -1,24 +1,25 @@
 #include "rnn.hpp"
 #include <iostream>
 #include <Eigen>
-#include <Eigen/Dense>
 #include <vector>
 #include <math.h>
 
 using namespace Eigen;
 
 // void getH(float, float, float*);
-void sigmoid(Eigen::Matrix2f &);
-void sigmoidInverse(Eigen::Matrix2f &m);
-void grutanh(Eigen::Matrix2f &);
+void sigmoid(Matrix<float, Dynamic, Dynamic> &m);
+void sigmoidInverse(Matrix<float, Dynamic, Dynamic> &m);
+void grutanh(Matrix<float, Dynamic, Dynamic> &);
+
 typedef struct gru_weights
 {
     // reset weights and bias
-    Eigen::Matrix2f w_r, u_r, b_r;
+    Matrix<float, Dynamic, Dynamic> w_r, u_r, b_r;
     // update weights and bias
-    Eigen::Matrix2f w_z, u_z, b_z;
+    Matrix<float, Dynamic, Dynamic> w_z, u_z, b_z;
     // hidden_hat weights and bias
-    Eigen::Matrix2f w_hh, u_hh, b_hh;
+    Matrix<float, Dynamic, Dynamic> w_hh, u_hh, b_hh;
+
 } gru_weights;
 
 int main()
@@ -30,36 +31,36 @@ int main()
     m(1, 1) = 4.0;
     std::cout << m << '\n';
     m = m * 2.0;
-    sigmoid(m);
+    // sigmoid(m);
     std::cout << m << '\n';
     return 0;
 }
 
-void getH(Eigen::Matrix2f &in, Eigen::Matrix2f &prev_hs, gru_weights &g, Eigen::Matrix2f &hs)
+void getH(Matrix2f &in, Matrix2f &prev_hs, gru_weights &g, Matrix2f &hs)
 {
-    Eigen::Matrix2f r = (g.w_r * in) + (g.u_r * prev_hs);
+    Matrix2f r = (g.w_r * in) + (g.u_r * prev_hs);
     sigmoid(r);
-    Eigen::Matrix2f z = (g.w_z * in) + (g.u_z * prev_hs);
+    Matrix2f z = (g.w_z * in) + (g.u_z * prev_hs);
     sigmoid(z);
     sigmoidInverse(z);
-    Eigen::Matrix2f hhat = (g.w_hh * in) + r.cwiseProduct(g.u_hh * prev_hs);
+    Matrix2f hhat = (g.w_hh * in) + r.cwiseProduct(g.u_hh * prev_hs);
     grutanh(hhat);
     hs = z.cwiseProduct(prev_hs) + z.cwiseProduct(hhat);
 }
 
-void sigmoid(Eigen::Matrix2f &m)
+void sigmoid(Matrix2f &m)
 {
     for (auto &a : m.reshaped())
         a = 1 / (1 + exp(-a));
 }
 
-void grutanh(Eigen::Matrix2f &m)
+void grutanh(Matrix2f &m)
 {
     for (auto &a : m.reshaped())
         a = tanh(a);
 }
 
-void sigmoidInverse(Eigen::Matrix2f &m)
+void sigmoidInverse(Matrix2f &m)
 {
     for (auto &a : m.reshaped())
         a = 1 - a;
