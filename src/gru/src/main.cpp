93f5eeec933ccb29e62f1c9bf9a5711f144d1020
Brodiejt <brodiejt@vcu.edu>
2023-08-14 11:58:48 -0400

    updated forward function

--- src/gru/src/main.cpp
+++ src/gru/src/main.cpp
@@ -10,6 +10,8 @@ using namespace Eigen;
 void sigmoid(MatrixXf &m);
 void sigmoidInverse(MatrixXf &m);
 void grutanh(MatrixXf &);
+void prepareBatch(MatrixXf &m);
+MatrixXf softmax(MatrixXf &m);
 
 typedef struct gru_weights
 {
@@ -23,23 +25,35 @@ typedef struct gru_weights
     MatrixXf w_hh, u_hh, b_hh;
     // Matrix<float, 1, Dynamic> b_hh;
 
+    // hidden states
+    MatrixXf h_prev, hs;
+
+    // output weights
+    MatrixXf w_y, b_y;
+
 } gru_weights;
 
 int main()
 {
-    Matrix2f m(2, 2);
-    m(0, 0) = 1.0;
-    m(0, 1) = 2.0;
-    m(1, 0) = 3.0;
-    m(1, 1) = 4.0;
+    MatrixXf m = MatrixXf::Random(1, 7);
+
     std::cout << m << '\n';
     m = m * 2.0;
-    // sigmoid(m);
+    sigmoid(m);
     std::cout << m << '\n';
+
+    const int batchSize = 2;
+    const int seq_len = 3;
+
+    m.resize(batchSize, seq_len);
+    m.transposeInPlace();
+
+    std::cout << m;
+
     return 0;
 }
 
-void getH(MatrixXf &in, MatrixXf &prev_hs, gru_weights &g, MatrixXf &hs)
+void getH(MatrixXf &in, MatrixXf &prev_hs, gru_weights &g)
 {
     MatrixXf r = (g.w_r * in) + (g.u_r * prev_hs);
     sigmoid(r);
@@ -48,7 +62,7 @@ void getH(MatrixXf &in, MatrixXf &prev_hs, gru_weights &g, MatrixXf &hs)
     sigmoidInverse(z);
     MatrixXf hhat = (g.w_hh * in) + r.cwiseProduct(g.u_hh * prev_hs);
     grutanh(hhat);
-    hs = z.cwiseProduct(prev_hs) + z.cwiseProduct(hhat);
+    g.hs = z.cwiseProduct(prev_hs) + z.cwiseProduct(hhat);
 }
 
 void sigmoid(MatrixXf &m)
@@ -68,6 +82,23 @@ void sigmoidInverse(MatrixXf &m)
     for (auto &a : m.reshaped())
         a = 1 - a;
 }
+
+MatrixXf softmax(MatrixXf &m1)
+{
+    MatrixXf m = m1;
+    float ymax = 0;
+    for (auto &a : m.reshaped())
+    {
+        ymax = (a > ymax) ? a : ymax;
+    }
+
+    for (auto &a : m.reshaped())
+    {
+        a = exp(a - ymax);
+    }
+    m /= m.sum();
+    return m;
+}
 void initialize_gru_weights(gru_weights &g, int h_size, int numClass, int batchSize)
 {
     // Weight matrices for inputs
@@ -75,8 +106,9 @@ void initialize_gru_weights(gru_weights &g, int h_size, int numClass, int batchS
     g.w_r = MatrixXf::Random(numClass, h_size);
     g.w_hh = MatrixXf::Random(numClass, h_size);
 
-    // hidden state
-    MatrixXf h_state = MatrixXf::Zero(batchSize, h_size);
+    // hidden states
+    g.hs = MatrixXf::Zero(batchSize, h_size);
+    g.h_prev = g.hs;
 
     // Weight matrices for hidden layer
     g.u_z = MatrixXf::Random(h_size, h_size);
@@ -84,17 +116,29 @@ void initialize_gru_weights(gru_weights &g, int h_size, int numClass, int batchS
     g.u_hh = MatrixXf::Random(h_size, h_size);
 
     // bias vectors
-    g.b_z = MatrixXf::Random(1,h_size);
-    g.b_r = MatrixXf::Random(1,h_size);
-    g.b_hh = MatrixXf::Random(1,h_size);
+    g.b_z = MatrixXf::Random(1, h_size);
+    g.b_r = MatrixXf::Random(1, h_size);
+    g.b_hh = MatrixXf::Random(1, h_size);
+
+    // output weights
+    g.w_y = MatrixXf::Random(h_size, numClass);
+    g.b_y = MatrixXf::Zero(numClass);
 }
 
 // Forward pass
-void gru(Matrix2f &in, int idden_size)
+void gru(MatrixXf &in, MatrixXf &h, gru_weights weights)
 {
-    Matrix2f outputs;
-    for (auto &sequence : in.reshaped())
+    MatrixXf y_t;
+    for (int i = 0; i < in.rows(); i++)
     {
-        // getH(sequence, , )
+
+        weights.h_prev = h;
+        getH(in, weights.h_prev, weights);
+
+        // Calc linear layer
+        MatrixXf y_linear = (weights.hs * weights.w_y) + weights.b_y;
+        // Calc y_t with softmax activation function
+
+        y_t = softmax(y_linear);
     }
 }
